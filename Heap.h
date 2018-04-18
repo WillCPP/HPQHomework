6bb7337496dc4467536d0f4d2571b95b3b49c7d2
“Stephanie <tamse@mail.uc.edu>
2018-04-18 18:23:10 +0000

    add heap.h

---
+++ Heap.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "node.h"
+
+class PriorityQueue
+{
+public:
+	PriorityQueue();
+	~PriorityQueue();
+
+	Node* head;
+private:
+
+};
+
+PriorityQueue::PriorityQueue()
+{
+}
+
+PriorityQueue::~PriorityQueue()
+{
+}
